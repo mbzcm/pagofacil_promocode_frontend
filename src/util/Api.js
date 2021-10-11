fa80852d35a99baaec4bfc917aee526f15085786
MigelBaez <miguel.baez@rimac.com.pe>
2021-10-11 08:19:08 -0500

    feat(frontend): init

--- src/util/Api.js
+++ src/util/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: "https://localhost:44332",
+  baseURL: process.env.REACT_APP_END_POINT,
   headers: {
     'Content-Type': 'application/json',
   }
